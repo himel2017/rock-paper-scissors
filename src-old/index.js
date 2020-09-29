10c94d2cf307d1d20474f4201005725a0cd156d9
Md. Rashedul Islam  Siraji <rashedul.accl@akij.net>
2020-09-29 18:43:38 +0600

    style change

--- src-old/index.js
+++ src-old/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Player from "./component/player";
-import rock from './assets/rock.png';
-import paper from './assets/paper.png';
 import "./styles.css";
 
 const weapons = ["rock", "paper", "scissors"];
@@ -116,7 +114,7 @@ class App extends Component {
     this.setState({
       playerOneScore,
       playerTwoScore,
-      // mode,
+      mode,
     });
   }
 
@@ -137,7 +135,7 @@ onChangeValue = (event) => {
     const { playerOne, playerTwo, winner } = this.state;
     return (
       <>
-        {/* <div className ="main_area">
+        <div className ="main_area">
           <div className="continer">
           <h1 className ="header_title" style={{ textAlign: "center" }}>Waste an hour having fun</h1>
 
@@ -157,7 +155,6 @@ onChangeValue = (event) => {
             <Player weapon={playerOne} />
             <Player weapon={playerTwo} />
           </div>
-
           {this.state.mode === "player" && (
             <>
             <div className ="pick_item">
@@ -182,10 +179,7 @@ onChangeValue = (event) => {
           </div>
             </>
           )}
-
-
-          <div className="winner">{winner ? this.selectWinner() : null}
-          </div>
+          {/* <div className="winner">{winner ? this.selectWinner() : null}</div> */}
           <button type="button" onClick={this.startGame}>
 
           Start!</button>
@@ -193,100 +187,8 @@ onChangeValue = (event) => {
           <button type="button" onClick={this.endtGame}>
             End/Reset</button>!
         </div>
-        </div> */}
-
-          {/* Rafiul code */}
-
-          <div className="manin_wrapper">
-              <div className="heading">
-                    <h1>Waste an hour having fun</h1>
-                    <h3>Score : 48</h3>
-              </div>
-              <div className="mode_heading">
-          <h3>Select Playing Mode:</h3>
-         
-              <div onChange={this.onChangeValue}>
-               <input type="radio" value="player" name="mode" checked={this.state.mode === "player"} /> Player VS Computer
-                <input type="radio" value="computer" name="mode" checked={this.state.mode === "computer"} /> Computer VS Computer
-              </div>
-              </div>
-
-
-              <div className="fullwrapper">
-                <div className="singlebox">
-                    {/* <img src={rock} /> */}
-                    <Player weapon={playerOne} />
-                    <h3>Player one<br/> ({this.state.mode === "player" ? "You" : "Computer"}): {this.state.playerOneScore} </h3>
-                   
-          
-                </div>
-                <div className="singlebox bo">
-                    {/* <img src={paper} /> */}
-                    <Player weapon={playerTwo} />
-                    <h3>Player Two (Computer): <br/> {this.state.playerTwoScore} </h3>
-                   
-                </div>
-                {/* <div>
-                  <Player weapon={playerOne} />
-                  <Player weapon={playerTwo} />
-                </div> */}
-              </div>
-
-              {this.state.mode === "player" && (
-            <>
-
-              <div className="buttons"> 
-                  <div className="bu">
-                  <button
-                      className="b1"
-                      onClick={() => this.selectWeapon("rock")}
-                    >
-                      rock
-                 </button>
-                 </div>
-                 <div className="bu">
-                 <button
-                      className="b1"
-                      onClick={() => this.selectWeapon("paper")}
-                    >
-                      Paper
-                 </button>
-                 </div>
-
-                 <div className="bu">
-                 <button
-                      className="b1"
-                      onClick={() => this.selectWeapon("scissors")}
-                    >
-                      Scissor
-                 </button>
-                 </div>
-
-                      {/* <button className="b1"> Rock</button>
-                  </div>
-                  <div className="bu">
-                      <button className="b1"> Paper </button>
-                  </div>
-                  <div className="bu">
-                      <button className="b1"> Scissor</button>
-                  </div> */}
-              </div>
-              </>
-          )}
-              <div className="start">
-                   <button className="startb" onClick={this.startGame}> Start</button>
-                   
-                   
-              </div>
-
-              <div className="end">
-                   <button className="endB" onClick={this.endtGame}> Reset</button>
-                   
-                   
-              </div>
-              </div>
-
-    </>
+        </div>
+      </>
     );
   }
 }
